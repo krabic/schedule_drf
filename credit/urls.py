8c01159f30adb7dfcba9410826857a4e60d0e5d5
krabic <grigoryvoloshyn@gmail.com>
2023-07-11 15:17:22 +0300

    fix

--- credit/urls.py
+++ credit/urls.py
@@ -2,7 +2,11 @@ from django.urls import path
 
 from .update_payment_schedule_views import PaymentScheduleUpdateView
 from .create_payment_schedule_views import PaymentScheduleCreateView
+"""
+http://127.0.0.1:8000/api/credit/create-payment-schedule/
 
+http://127.0.0.1:8000/api/credit/update-payment-schedule/
+"""
 urlpatterns = [
 
     path('create-payment-schedule/', PaymentScheduleCreateView.as_view(http_method_names=['post']),
